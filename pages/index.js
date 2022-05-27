135e528e502d89f0400a2fc3111763edea13783e
Filipe <filipemard@gmail.com>
2022-05-26 23:16:07 -0300

    create the fetch of pokeapi

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
-export default function Home() {
+export async function getStaticProps(){
+  const maxPokemons = 251, //Numero maximo de pokemons que podem ser carregados
+  api = "https://pokeapi.co/api/v2/pokemon/"
+
+  const res = await fetch(`${api}?limit=${maxPokemons}`),
+  data = await res.json()
+
+  //add pokemon id
+  data.results.forEach((item,index) => {
+    item.id = index + 1
+  });
+
+  return {
+    props: {
+      pokemons : data.results
+    }
+  }
+}
+
+export default function Home({pokemons}) {
   return (
     <div className="">
-      <h1 className="text-red-500">Hello World</h1>
+      <h1 className="text-red-500">Pokemons</h1>
+      <ul>
+        {pokemons.map(pokemon => {
+          <li key={pokemon.id}>{pokemon.name}</li>
+        })}
+      </ul>
     </div>
   )
 }
